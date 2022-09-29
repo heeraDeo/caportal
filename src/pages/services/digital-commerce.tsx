e8f4f3d2f1e14550748d495cf06243a3129e21b2
victor <victor@victors-MacBook-Pro.local>
2022-09-29 23:15:40 +0530

    boliler

---
+++ src/pages/services/digital-commerce.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function DigitalCommerce() {
+  return (
+    <div>digital-commerce</div>
+  )
+}
---
+++ src/pages/services/digital-transformation-consulting.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function DigitalTransformationConsulting() {
+  return (
+    <div>digital-transformation-consulting</div>
+  )
+}
